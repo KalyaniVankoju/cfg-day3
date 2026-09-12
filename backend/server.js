069b2c71589132c812c47acd10529e92452421dc
Kalyani Vankoju <kalyanivankoju07@gmail.com>
2026-09-12 17:07:51 +0530

    Changed the server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use('/api/students', studentRoutes);
 const startServer = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_URI);
-		app.listen(PORT, '0.0.0.0', () => {
+		app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`)
 })
 	} catch (error) {
